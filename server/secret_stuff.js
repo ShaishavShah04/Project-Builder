0165a0cb16c36938aecda4b8091c5860fa09d3d2
Shaishav Shah <38801683+ShaishavShah04@users.noreply.github.com>
2021-12-04 12:41:28 -0700

    Delete secret_stuff.js

--- server/secret_stuff.js
+++
@@ -1,5 +0,0 @@
-export const HOST="localhost";
-export const PORT=5000;
-export const SECRET_KEY =  "mySecretKey";
-export const REFRESH_KEY =  "mySecretKey";
-export const DB_URL = "mongodb+srv://shaishav:mernstack@cluster0.8ctsf.mongodb.net/ProjectBuildR?retryWrites=true&w=majority";
